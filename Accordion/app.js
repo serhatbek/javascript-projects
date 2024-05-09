eaef22c0d5359061218956b0f27a5dd39de9d276
EXT02D52540 <serhat.bek@consultant.turkcell.com.tr>
2024-05-09 22:34:37 +0300

    accordion functionality

--- Accordion/app.js
+++ Accordion/app.js
@@ -1,1 +1,18 @@
-console.log('accordion');
+const accordionItems = document.querySelectorAll('.accordion__item');
+
+if (accordionItems) {
+  accordionItems.forEach((accordion) => {
+    accordion.addEventListener('click', (e) => {
+      let target = e.currentTarget;
+
+      if (!accordion.classList.contains('active')) {
+        accordionItems.forEach((item) => {
+          item.classList.remove('active');
+        });
+        accordion.classList.add('active');
+      } else {
+        accordion.classList.remove('active');
+      }
+    });
+  });
+}
