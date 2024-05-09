b78b581d35df84272f6dd1151453a8a4ba31238f
EXT02D52540 <serhat.bek@consultant.turkcell.com.tr>
2024-05-10 01:13:14 +0300

    add readme

---
+++ Accordion/README.md
@@ -0,0 +1,7 @@
+# Accordion
+
+<figure class="video_container">
+ <video controls="true" allowfullscreen="true">
+ <source src="./accordion.mp4" type="video/mp4">
+ </video>
+</figure>
--- Accordion/app.js
+++ Accordion/app.js
@@ -2,17 +2,19 @@ const accordionItems = document.querySelectorAll('.accordion__item');
 
 if (accordionItems) {
   accordionItems.forEach((accordion) => {
-    accordion.addEventListener('click', (e) => {
-      let target = e.currentTarget;
-
-      if (!accordion.classList.contains('active')) {
-        accordionItems.forEach((item) => {
-          item.classList.remove('active');
-        });
-        accordion.classList.add('active');
-      } else {
-        accordion.classList.remove('active');
-      }
-    });
+    accordion.addEventListener('click', (e) => toggleAccordion(e));
   });
 }
+
+const toggleAccordion = (e) => {
+  let target = e.currentTarget;
+
+  if (!target.classList.contains('active')) {
+    accordionItems.forEach((item) => {
+      item.classList.remove('active');
+    });
+    target.classList.add('active');
+  } else {
+    target.classList.remove('active');
+  }
+};
