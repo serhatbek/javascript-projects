11514ec32367b423b941a9dd06a7b081b398042d
serhatbek <serhat.bekk@gmail.com>
2024-05-21 23:44:55 +0300

    tooltip js

--- Tooltip/app.js
+++ Tooltip/app.js
@@ -1,1 +1,28 @@
-console.log('tooltip');
+const triggers = document.querySelectorAll('.trigger');
+
+const openTooltip = (e) => {
+  const trigger = e.target;
+  const tooltip = trigger.querySelector('[role=tooltip]');
+  tooltip.classList.add('active');
+};
+
+const closeTooltip = (e) => {
+  const trigger = e.target;
+  const tooltip = trigger.querySelector('[role=tooltip]');
+  tooltip.classList.remove('active');
+};
+
+if (triggers) {
+  triggers.forEach((trigger) => {
+    let tooltip = document.createElement('span');
+
+    tooltip.setAttribute('role', 'tooltip');
+    tooltip.setAttribute('inert', true);
+    tooltip.textContent = trigger.dataset.tooltip;
+
+    trigger.appendChild(tooltip);
+
+    trigger.addEventListener('mouseenter', openTooltip);
+    // trigger.addEventListener('mouseleave', closeTooltip);
+  });
+}
