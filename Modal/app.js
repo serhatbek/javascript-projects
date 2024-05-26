aa373611d01933b26bda8133f56748cf41a5ece2
serhatbek <serhat.bekk@gmail.com>
2024-05-27 00:17:57 +0300

    modal javascript

--- Modal/app.js
+++ Modal/app.js
@@ -1,1 +1,28 @@
-console.log(modal);
+document.addEventListener('DOMContentLoaded', () => {
+  const modal = document.querySelector('.js-modal');
+  const showModalBtn = document.querySelector('.js-show-modal');
+  const closeModalBtns = document.querySelectorAll('.js-close-modal');
+
+  const closeModal = () => {
+    modal.classList.remove('modal--opened');
+  };
+
+  const showModal = () => {
+    modal.classList.add('modal--opened');
+  };
+
+  if (modal) {
+    showModalBtn.addEventListener('click', showModal);
+    closeModalBtns.forEach((btn) => btn.addEventListener('click', closeModal));
+  }
+
+  document.addEventListener('click', (event) => {
+    if (
+      modal.classList.contains('modal--opened') &&
+      !event.target.closest('.modal__content') &&
+      !event.target.closest('.js-show-modal')
+    ) {
+      closeModal();
+    }
+  });
+});
