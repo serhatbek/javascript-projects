03d4f6ee55b218dd647eb89a8a0cbbcb0deded51
serhatbek <serhat.bekk@gmail.com>
2024-05-30 19:49:46 +0300

    burger menu functionality

--- BurgerMenu/app.js
+++ BurgerMenu/app.js
@@ -1,1 +1,12 @@
-console.log('burger menu');
+document.addEventListener('DOMContentLoaded', () => {
+  const burgerBtn = document.querySelector('.hamburger-menu');
+
+  const toggleMobileMenu = () => {
+    burgerBtn.classList.toggle('hamburger-menu--active');
+    /* you can add your mobile menu functionality here */
+  };
+
+  if (burgerBtn) {
+    burgerBtn.addEventListener('click', toggleMobileMenu);
+  }
+});
