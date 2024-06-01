fc333981e882c0eeedcf5e52177bb0e022c12acf
serhatbek <serhat.bekk@gmail.com>
2024-06-01 21:32:46 +0300

    close modal when esc clicked

--- Modal/app.js
+++ Modal/app.js
@@ -7,11 +7,19 @@ document.addEventListener('DOMContentLoaded', () => {
   const closeModal = () => {
     modal.classList.remove('modal--opened');
     body.classList.remove('overflowHidden');
+    document.removeEventListener('keydown', handleEscClose);
   };
 
   const openModal = () => {
     modal.classList.add('modal--opened');
     body.classList.add('overflowHidden');
+    document.addEventListener('keydown', handleEscClose);
+  };
+
+  const handleEscClose = (e) => {
+    if (e.key === 'Escape') {
+      closeModal();
+    }
   };
 
   if (modal) {
